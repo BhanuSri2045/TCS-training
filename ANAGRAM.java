862d022b934aed836f6bb65730fa02db111ce3b8
BhanuSri2045 <bhanusri2045@gmail.com>
2026-07-07 12:06:19 +0530

    Add files via upload

---
+++ ANAGRAM.java
@@ -0,0 +1,19 @@
+public class ANAGRAM{
+public static boolean isanagram(String s1, String s2){
+int[]arr = new int [26];
+if(s1.length()!=s2.length())return false;
+for(int i=0; i<s1.length();i++){
+arr[s1.charAt(i)-'a']++;
+arr[s2.charAt(i)-'a']--;
+}for(int i:arr){
+if(i!=0)return false;
+}
+return true;
+
+}
+public static void main (String args[]){
+String s1 = "eat";
+String s2 = "ate";
+System.out.println(isanagram(s1,s2)? "Anagram":"Not an angram");
+}
+}
